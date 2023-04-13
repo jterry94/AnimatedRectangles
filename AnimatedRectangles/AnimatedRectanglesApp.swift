f22a94ded5e5a45841096428a7c595ba2490a358
jterry94 <terryj@iit.edu>
2023-04-13 11:41:49 -0500

    Initial Commit

---
+++ AnimatedRectangles/AnimatedRectanglesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedRectanglesApp.swift
+//  AnimatedRectangles
+//
+//  Created by Jeff_Terry on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedRectanglesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
