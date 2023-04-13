54ab94a3dfc36934d15d41c605414af57f4482ee
jterry94 <terryj@iit.edu>
2023-04-13 15:40:07 -0500

    Working 2D array of animated rectangles.

--- AnimatedRectangles/ContentView.swift
+++ AnimatedRectangles/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @StateObject private var twoDMagnet = TwoDMagnet()
     let upColor = Color(red: 0.25, green: 0.5, blue: 0.75)
-    let downColor = Color(red: 0.275, green: 0.5, blue: 0.25)
+    let downColor = Color(red: 0.75, green: 0.5, blue: 0.25)
     let spinWidth = 25.0
 
     var body: some View {
@@ -21,7 +21,7 @@ struct ContentView: View {
                     
                     for spin in twoDMagnet.spins {
                         let rect = CGRect(x: spin.x * (size.width/CGFloat(spinWidth)), y: spin.y * (size.height/CGFloat(spinWidth)), width: (size.height/CGFloat(spinWidth)), height: (size.height/CGFloat(spinWidth)))
-                        let shape = Capsule().path(in: rect)
+                        let shape = Rectangle().path(in: rect)
                         if (spin.spin){
                             context.fill(shape, with: .color(upColor))}
                         else{
@@ -31,9 +31,10 @@ struct ContentView: View {
                     }
                 }
             }
+            
             .background(.black)
             .ignoresSafeArea()
-            
+            .padding()
             
             Button("Start", action: setupSpins)
         }
@@ -72,53 +73,11 @@ class TwoDMagnet: ObservableObject {
     func update(to date: Date) {
         for i in 0..<spins.count {
             
-           // let x = spins[i].x
-            //let y = spins[i].y
-            
             spins[i].spin = Bool.random()
         }
     }
 }
 
-
-
-//
-//struct ContentView: View {
-//    @StateObject private var storm = Storm()
-//    let rainColor = Color(red: 0.25, green: 0.5, blue: 0.75)
-//
-//    var body: some View {
-//        TimelineView(.animation) { timeline in
-//            Canvas { context, size in
-//                storm.update(to: timeline.date)
-//
-//                for drop in storm.drops {
-//                    let age = timeline.date.distance(to: drop.removalDate)
-//                    let rect = CGRect(x: drop.x * size.width, y: size.height - (size.height * age * drop.speed), width: 2, height: 10)
-//                    let shape = Capsule().path(in: rect)
-//                    context.fill(shape, with: .color(rainColor))
-//                }
-//            }
-//        }
-//        .background(.black)
-//        .ignoresSafeArea()
-//    }
-//}
-//
-//struct Raindrop: Hashable, Equatable {
-//    var x: Double
-//    var removalDate: Date
-//    var speed: Double
-//}
-//
-//class Storm: ObservableObject {
-//    var drops = Set<Raindrop>()
-//
-//    func update(to date: Date) {
-//        drops = drops.filter { $0.removalDate > date }
-//        drops.insert(Raindrop(x: Double.random(in: 0...1), removalDate: date + 1, speed: Double.random(in: 1...2)))
-//    }
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
